8c6d71b3287e6fd6655569a285363fd721836f20
rochel-05 <58344884+rochel-05@users.noreply.github.com>
2019-12-22 20:14:40 +0400

    Update run.py

--- run.py
+++ run.py
@@ -116,4 +116,4 @@ def page_not_found(error):
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5800)
